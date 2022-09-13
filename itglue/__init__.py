0df90bc66cef6f3e72a76ed0d3aa8341bab3a961
David Haymond <david.haymond@lewiscountywa.gov>
2022-09-13 12:22:31 -0700

    Add Flit pyproject.toml, bump version to 0.2.0

--- itglue/__init__.py
+++ itglue/__init__.py
@@ -1,3 +1,7 @@
+"""A simple wrapper for the IT Glue API"""
+
 from .connection import connection
 from .path_processor import process_path, PathProcessor
 from .resources import *
+
+__version__ = '0.2.0'
